f96e3cd9efbd74da9a3432f19e5fcabb5466ad26
Miranda Pepin <35225014+mimip125@users.noreply.github.com>
2018-07-05 13:45:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function currentSlide(n) {
 function showSlides(n) {
   var  i;
   var slides = document.getElementsByClassName("mySlides");
-   var dots = document.getElementsByClassName("demo");
+  var dots = document.getElementsByClassName("demo");
   var captionText  = document.getElementById("caption");
   if (n > slides.length) {slideIndex  = 1}
   if (n < 1) {slideIndex = slides.length}
@@ -24,7 +24,7 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
      dots[i].className = dots[i].className.replace(" active", "");
   }
-   slides[slideIndex-1].style.display = "block";
+  slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className  += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
 } 
