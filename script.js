3400e0b911c5014791b283f78b8327b6de87527d
Miranda Pepin <35225014+mimip125@users.noreply.github.com>
2018-07-14 13:09:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -27,5 +27,12 @@ function showSlides(n) {
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
 
-  
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
   
